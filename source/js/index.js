023c782315da7402d03abdee4d547fbe1b663718
gigu007 <gideonukaegbu007@gmail.com>
2022-02-21 14:07:34 +0100

    first commit

---
+++ source/js/index.js
@@ -0,0 +1,40 @@
+//Varables
+const menu = document.querySelector('#bgsmartPhone'),
+      hamburger = document.querySelector('#bar-menu'),
+      close_menu=document.querySelector('#closeMenu');
+
+      console.log(menu)
+;
+
+// Listeners
+loadListener();
+
+
+function loadListener() {
+    hamburger.addEventListener('click', () => {
+        console.log('clicked..')
+        menu.style.display = 'block';
+        close_menu.style.display='block';
+        hamburger.style.display='none'
+
+    })
+    close_menu.addEventListener('click',()=>{
+        console.log('clicked');
+        menu.style.display = 'none';
+        close_menu.style.display='none';
+        hamburger.style.display='block'
+
+    })
+}
+var luna, lunamodal,
+lunamodal = document.querySelector('#lunamodal');
+
+
+luna=document.querySelector('#modalBtn').addEventListener('click', function() {
+    lunamodal.style.display = 'block';
+    console.log('clicked...');
+});
+
+document.querySelector('.grey-btn a').addEventListener('click', () => {
+    let x = document.querySelector('#lunamodal').style.display = 'none';
+})
